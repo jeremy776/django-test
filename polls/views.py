0e30d1da08ffa63d133a03ddb9db29cede70e08b
jeremy776 <jeremy.kusuma2018@gmail.com>
2021-10-16 15:37:59 +0700

    ik

--- polls/views.py
+++ polls/views.py
@@ -12,7 +12,7 @@ def index(request):
   return render(request, "polls/index.html", context)
   
 def detail(request, question_id):
-  question = get_object_or_404(Question, pk=question_id)
+  question = Question.objects.get(question_id)
   print(question)
   return render(request, "polls/detail.html", { question: question })
   
