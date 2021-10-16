eb7fea82e01f71e359280533572e1dfdc56db040
jeremy776 <jeremy.kusuma2018@gmail.com>
2021-10-16 09:56:42 +0700

    ok

--- polls/models.py
+++ polls/models.py
@@ -6,6 +6,9 @@ class Question(models.Model):
   question_text = models.CharField(max_length=200)
   pub_date = models.DateTimeField('date_published')
   
+  def was_published_recently(self):
+    return self.pub_date >= timezone.now() - datetime.timedelta(days=-1)
+
   def __str__(self):
     return self.question_text
 
@@ -14,8 +17,5 @@ class Choice(models.Model):
   choice_text = models.CharField(max_length=200)
   vote = models.IntegerField(default=0)
   
-  def was_published_recently(self):
-    return self.pub_date >= timezone.now() - datetime.timedelta(days=-1)
-
   def __str__(self):
     return self.choice_text
