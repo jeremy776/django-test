dad558a36dca9d0aeb5ced1bfc1edc06c75f0bb3
jeremy776 <jeremy.kusuma2018@gmail.com>
2021-10-16 09:15:36 +0700

    g

--- firstsite/urls.py
+++ firstsite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-  path("polls/", include("polls.url")),
+  path(r"^polls/", include("polls.url")),
   path('admin/', admin.site.urls),
 ]
