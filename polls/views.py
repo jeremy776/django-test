cf16e9a68e4b61d43992c5529856b01599432f64
jeremy776 <jeremy.kusuma2018@gmail.com>
2021-10-16 16:26:38 +0700

    k

--- polls/views.py
+++ polls/views.py
@@ -6,22 +6,19 @@ from .models import Question
 
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  return render(request, "polls/index.html", { latest_question_list: latest_question_list })
+  list_question = Question.objects.all()
+  return render(request, "polls/index.html", { list_question: list_question, latest_question_list: latest_question_list })
   
 def detail(request, question_id):
   try:
     question = Question.objects.get(pk=question_id)
   except Question.DoesNotExist:
     raise Http404("tidak ada");
-    
-  print(question.id)
-  print("==============")
-  print(question.question_text)
   return render(request, "polls/detail.html", { 'question': question })
   
 def results(request, question_id):
   response = "You're looking at the results of question %s."
-  return HttpResponse(response % question.id)
+  return HttpResponse(response % question_id)
   
 def vote(request, question_id):
   return HttpResponse("You're voting for question %s." % question_id)
